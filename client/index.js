42255381318cfe459ce075d345e07a991f302ecc
Payam Beigi <beigi.payam@gmail.com>
2022-12-15 23:38:35 -0800

    implement client side Merkle tree proof

--- client/index.js
+++ client/index.js
@@ -5,10 +5,22 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  //Prove to the server the name is in the list
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+  //Import names to the Merkle tree and get the root hash(HEX)
+  const merkleTree = new MerkleTree(niceList);
+  const root = merkleTree.getRoot();
+
+  // Retrieve the proof that the name block is in the list 
+const name = 'Payam Beigi';
+const index = niceList.findIndex(n => n === name);
+const proof = merkleTree.getProof(index);
+
+  const { data: gift } = await axios.post(`${serverUrl}/gift` ,{
+    clientProof: proof,
+  })
+  .catch(function (error) {
+    console.log(error);
   });
 
   console.log({ gift });
